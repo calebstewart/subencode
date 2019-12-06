e98baeb24f5fae45bd761c7cafaab3578868c246
Caleb Stewart <caleb.stewart94@gmail.com>
2019-12-06 17:49:22 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,61 @@
+# `subencode.py`
+
+When producing binary exploits (e.g. buffer overflows), you often run into
+restrictive environments which only permit certain bytes (e.g. alphanumeric).
+One method to get around this is to "sub-encode" your data, and push it onto the
+stack at runtime. For example, to push the value `0xDEADBEEF` onto the stack,
+we could do the following uing only alphanumeric shellcode:
+
+```asm
+and eax,0x554e4d4a				# Zero out EAX
+and eax,0x2a313235
+
+sub eax,0x11292109				# 
+sub eax,0x10292008
+```
+
+This shellcode assembles to the following: `%JMNU%521*-\t!)\x11-\x08 )\x10`. You
+can easily see how this works in python:
+
+```python
+>>> (0-0x11292109) & 0xFFFFFFFF
+4007059191
+>>> (_-0x10292008) & 0xFFFFFFFF
+3735928559
+>>> hex(_)
+'0xdeadbeef'
+>>>
+```
+
+This script will automatically calculate the required sub-encoded bytes to
+properly load the given data into memory in a restrictive environment. The
+algorithm used here is derived from various articles, but as an example, a good
+article can be read [here](https://marcosvalle.github.io/re/exploit/2018/10/05/sub-encoding.html).
+
+## Usage
+
+```
+usage: subencode.py [-h] [--input INPUT] (--badbytes BADBYTES | --goodbytes GOODBYTES) [--max-div MAX_DIV]
+
+Sub-encode the given data for use in exploits with restrictive bad bytes
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --input INPUT, -i INPUT
+                        File which contains the data to be encoded
+  --badbytes BADBYTES, -b BADBYTES
+                        A list of bad bytes. \x encodings are allowed.
+  --goodbytes GOODBYTES, -g GOODBYTES
+                        A list of allowed bytes. \x encodings are allowed
+  --max-div MAX_DIV, -m MAX_DIV
+                        Maximum number of divisions to find sub-encoding (default: 10)
+```
+
+## Example
+
+```
+$ ./subencode.py -g "\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0b\x0c\x0e\x0f\x10\x11\x12\x13\x14\x15\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x30\x31\x32\x33\x34\x35\x36\x37\x38\x39\x3b\x3c\x3d\x3e\x41\x42\x43\x44\x45\x46\x47\x48\x49\x4a\x4b\x4c\x4d\x4e\x4f\x50\x51\x52\x53\x54\x55\x56\x57\x58\x59\x5a\x5b\x5c\x5d\x5e\x5f\x60\x61\x62\x63\x64\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74\x75\x76\x77\x78\x79\x7a\x7b\x7c\x7d\x7e\x7f" < deadbeef.bin
+chunks = [
+    [0x11292109, 0x10292008],
+]
+```
---
+++ subencode.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python3
+from pwn import *
+import argparse
+import itertools
+import resource
+import sys
+
+
+class EncodingFailure(Exception):
+    """ This is raised when a byte cannot be encoded automatically """
+
+    pass
+
+
+class DivisionFailed(Exception):
+    """ This simply notifies encode_byte that check_div failed """
+
+    pass
+
+
+class BiggerDivision(Exception):
+    """ This is raised when the located encoded has a larger value than the
+    currently used encoding. It triggers encode_chunk to start over with a new
+    minimum division size. """
+
+    def __init__(self, div, *args, **kwargs):
+        super(BiggerDivision, self).__init__(*args, **kwargs)
+        self.div = div
+
+
+def check_div(x, div, good_bytes, carry=False):
+    """ Check if this division works. This will try to take the target value `x`
+    and divide it into `div` sections which add up to `x` and are each themselves
+    in the given `good_bytes` byte string.
+
+    On success, this function returns a tuple of (carry_flag, value_array) where
+    carry is set to true, if we had to increment `x` by 0x100, triggering a carry
+    on the next byte.
+
+    On failure, this function raises "DivisionFailed".
+    """
+
+    # Divide by the divisor
+    v = x // div
+    m = x % div
+
+    # This is likely caused by a carry with a division that is too small
+    if v >= 256:
+        raise DivisionFailed
+
+    # Easy, no modulos
+    if mod == 0 and v in good_bytes:
+        return [v] * n
+
+    # Find all possible v modifications
+    offsets = [v - c for c in good_bytes]
+
+    # How many entries do we want to split this into?
+    for split in range(1, div):
+        # Iterate over all possibilities
+        for offset_list in itertools.combinations_with_replacement(offsets, split):
+            # Check if this works
+            if sum(offset_list) == m:
+                return (
+                    carry,
+                    [v + offset for offset in offset_list] + [v] * (div - split),
+                )
+
+    # If we've already carried, there's nothing we can do
+    if carry:
+        raise DivisionFailed
+
+    # Try to use the carry bit to our advantage
+    return check_div(x + 0x100, div, good_bytes, carry=True)
+
+
+def encode_byte(x, good_bytes, min_div=0, max_div=-1):
+    """ This function attempts to divide `x` into the minimum number of
+    sub-encodings possible, starting at `min_div`.
+
+    If `min_div` is zero, it will search from div=1 to div=max_div. If `min_div`
+    is anything other than zero, it will start at `min_div`. If `min_div` does not
+    work, but a higher div does, BiggerDivision is raised with the parameter being
+    the correct division size.
+    """
+
+    # If min_div is uninitialized, we start at 2
+    if min_div == 0:
+        start_div = 1
+    else:
+        # Otherwise, start at the minimum division
+        start_div = min_div
+
+    # Loop up to the maximum division size
+    for div in range(start_div, max_div & 0xFFFFFFFF):
+        try:
+            # Try to find valid values with this division
+            carry, v = check_div(x, div, good_bytes)
+
+            # This means that previous encodings are wrong (or at least contain
+            # null bytes). It triggers encode_chunk to start over with this as
+            # a new minimum division value.
+            if min_div != 0 and div > min_div:
+                raise BiggerDivision(div)
+
+            # All good, return the value
+            return carry, v
+        except DivisionFailed:
+            # check_div failed, that's fine. Continue looking for a good
+            # division.
+            continue
+    else:
+        # If we complete this loop, then we did not find any good divisions.
+        # This script doesn't know how to encode this byte with the given
+        # division restrictions :(
+        raise EncodingFailure
+
+
+def encode_chunk(chunk, good_bytes, max_div=-1):
+    """ Produce sub-encoding bytes for the given value. 
+    """
+
+    # Calculate a complement such that (0-X) & 0xFFFFFFFF = chunk
+    X = ((0xFFFFFFFF + 1) - chunk) & 0xFFFFFFFF
+
+    # Initialize the encodings result, carry flag, and division size
+    encodings = []
+    carry = 0
+    div = 0
+
+    # We continue trying until we receive an EncodingFailure exception which is
+    # passed back to the caller
+    while True:
+
+        try:
+            # Iterate over each byte (least to most significant: important for
+            # carry!)
+            for i, x in enumerate(p32(X)):
+
+                # Account for any carry from previous encoding
+                if carry:
+                    x = x - 1
+
+                # Encode this byte
+                carry, values = encode_byte(x, good_bytes, min_div=div, max_div=max_div)
+
+                # Initialize encodings matrix if this is the first result
+                if div == 0:
+                    div = len(values)
+                    encodings = [[] for i in range(div)]
+
+                # Add these values to the encoding matrix
+                for i, v in enumerate(values):
+                    encodings[i].append(v)
+            else:
+                # We successfully encoded all bytes! Now we can decode them
+                # as integers and return!
+                encodings_final = [u32(bytes(v)) for v in encodings]
+                return encodings_final
+
+        except BiggerDivision as e:
+            # A single byte increased the size of our encodings.
+            # Re-initialize the encodings and start over with a new minimum
+            # division. This assumes that a null byte is a bad byte, but that's
+            # pretty common...
+            div = e.div
+            encodings = [[] for i in range(div)]
+
+
+def verify_chunk(chunk, encodings):
+    value = 0
+    for encoding in encodings:
+        value = (value - encoding) & 0xFFFFFFFF
+    return chunk == value
+
+
+if __name__ == "__main__":
+
+    # Build argument parser
+    parser = argparse.ArgumentParser(
+        description="Sub-encode the given data for use in exploits with restrictive bad bytes"
+    )
+    parser.add_argument(
+        "--input", "-i", default="-", help="File which contains the data to be encoded"
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument(
+        "--badbytes", "-b", help="A list of bad bytes. \\x encodings are allowed."
+    )
+    group.add_argument(
+        "--goodbytes", "-g", help="A list of allowed bytes. \\x encodings are allowed"
+    )
+    parser.add_argument(
+        "--max-div",
+        "-m",
+        type=int,
+        default=10,
+        help="Maximum number of divisions to find sub-encoding (default: 10)",
+    )
+    args = parser.parse_args()
+
+    # Grab good bytes list
+    if args.goodbytes:
+        good_bytes = bytes(args.goodbytes, "utf-8").decode("unicode_escape")
+        good_bytes = bytes(good_bytes, "utf-8")
+    else:
+        bad_bytes = bytes(args.badbytes, "utf-8").decode("unicode_escape")
+        good_bytes = bytes([c for c in range(256) if c not in bad_bytes])
+
+    # Open and read data
+    if args.input == "-":
+        data = sys.stdin.buffer
+    else:
+        data = open(args.input, "rb")
+
+    chunks = []
+    offset = 0
+
+    while True:
+
+        chunk = data.read(4)
+        if chunk == b"" or chunk is None:
+            break
+
+        chunk = chunk.ljust(4, b"\x00")
+
+        # Find the integer equivalent
+        chunk = u32(chunk)
+
+        try:
+            encodings = encode_chunk(chunk, good_bytes, max_div=args.max_div)
+        except EncodingFailure:
+            log.error("Failed to encode chunk at offset {offset:x}: {chunk:08x}")
+
+        if not verify_chunk(chunk, encodings):
+            log.error(
+                "Chunk verification failed at offset {offset:x}: {chunk:08x}->{encodings}"
+            )
+
+        chunks.append(encodings)
+
+    # Build output
+    result = ["chunks = ["]
+    for chunk in chunks:
+        result.append(f"    {[hex(c) for c in chunk]},".replace("'", ""))
+    result.append("]")
+    result = "\n".join(result)
+
+    # print it
+    print(result)
